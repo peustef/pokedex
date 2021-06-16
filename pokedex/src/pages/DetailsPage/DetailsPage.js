2f1bf644ce4a76235894014e7927bfd733505c9c
Vitor Suzuki <vitor.szk@outlook.com>
2021-06-16 20:07:58 -0300

    Layout da Página de Detalhes iniciado, sem funcionalidades e requisições

--- pokedex/src/pages/DetailsPage/DetailsPage.js
+++ pokedex/src/pages/DetailsPage/DetailsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../img/Logo.png";
 import { ImgContainer, Img } from "./styled";
 import '../../App.css';
@@ -8,7 +8,7 @@ import Button from '@material-ui/core/Button';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 import DeleteIcon from '@material-ui/icons/Delete';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import BulbaFront from '../../img/BulbaFront.png'
 
 
 const StyledButton = withStyles({
@@ -39,42 +39,76 @@ const DetailsPage = () => {
   const classes = useStyles();
 
   return (
-    <header>
-      <div className="Logo">
-        <ImgContainer>
-          <Img src={Logo}></Img>
-        </ImgContainer>
-        <h1 onClick={() => goToPokedex(history)}>POKÉDEX</h1>
-        <div className="Title">
-          <p className="TitleHeader">NOME DE POKEMON</p>
-          <div className="ButtonsContainer">
-            <Button
-              size="medium"
-              variant="contained"
-              color="default"
-              className={classes.button}
-              endIcon={<Icon>send</Icon>}
-            >
-              ADICIONAR
-            </Button>
-            <Button
-              size="medium"
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              endIcon={<DeleteIcon />}
-            >
-              REMOVER
-            </Button>
-            <StyledButton className="PHeader"
-              onClick={() => goBack(history)}>
-              VOLTAR
-            </StyledButton>
+    <div>
+      <header>
+        <div className="Logo">
+          <ImgContainer>
+            <Img src={Logo}></Img>
+          </ImgContainer>
+          <h1 onClick={() => goToPokedex(history)}>POKÉDEX</h1>
+          <div className="Title">
+            <p className="TitleHeader">NOME DE POKEMON</p>
+            <div className="ButtonsContainer">
+              <Button
+                size="medium"
+                variant="contained"
+                color="default"
+                className={classes.button}
+                endIcon={<Icon>send</Icon>}
+              >
+                ADICIONAR
+              </Button>
+              <Button
+                size="medium"
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                endIcon={<DeleteIcon />}
+              >
+                REMOVER
+              </Button>
+              <StyledButton className="PHeader"
+                onClick={() => goBack(history)}>
+                VOLTAR
+              </StyledButton>
+            </div>
           </div>
         </div>
-      </div>
 
-    </header>
+      </header>
+      <div className="ContainerDetails">
+        <div className="ContainerImgs">
+          <div className="PokeType">
+            <h3>Tipo 1</h3>
+            <h3>Tipo 2</h3>
+          </div>
+          <div className="ContainerImgFront">
+            <img className="ImgFront" src={BulbaFront} />
+          </div>
+          <div className="ContainerImgBack">
+            <img className="ImgBack" src={BulbaFront} />
+          </div>
+        </div>
+        <div className="PokeStats">
+          <h1>Poderes</h1>
+          <p>hp:</p>
+          <p>attack:</p>
+          <p>defense:</p>
+          <p>special-attack:</p>
+          <p>special-defense:</p>
+          <p>speed:</p>
+        </div>
+
+        <div className="PokeMoves">
+          <h1>Principais Ataques</h1>
+          <p>swords-dance</p>
+          <p>cut</p>
+          <p>bind</p>
+          <p>vine-whip</p>
+          <p>headbutt</p>
+        </div>
+      </div>
+    </div>
   );
 };
 

f87e3639cab80bd723dbbd9f70fc9fd67175f6e0
peustef <peustefanis@gmail.com>
2021-06-19 23:14:20 -0300

    conflitos resolvidos

--- pokedex/src/pages/PokedexPage/PokedexPage.js
+++ pokedex/src/pages/PokedexPage/PokedexPage.js
@@ -7,26 +7,6 @@ import { goToHome, goToPokedex } from "../../routes/coordinator";
 import PokeCard from "../../components/PokeCard/PokeCard";
 
 
-const StyledButton = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #282c34 30%, #444b59 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 36.5,
-    padding: '0 15px',
-    marginLeft: '70px',
-    boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .3)',
-    transition: '0.3s ease-in-out',
-    ['@media (max-width:768px)']: {
-      display: 'none',
-    },
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
-})(Button);
-
 
 const PokedexPage = (props) => {
   const history = useHistory()
